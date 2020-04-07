2b965f51254338e96cb8799aabc021c9009fa02d
mysolution20 <mysolution@op.pl>
2020-04-07 20:14:35 +0200

    initial commit

---
+++ src/main/java/com/kodilla/KodillaBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaBootcampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaBootcampApplication.class, args);
+    }
+
+}
