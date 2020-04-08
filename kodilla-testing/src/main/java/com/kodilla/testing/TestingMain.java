e7788f485b98b90edf4d183d99af3ec447e9d45d
mysolution20 <mysolution@op.pl>
2020-04-08 15:23:36 +0200

    Adding new tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+
+        System.out.println("*************************************************");
+        System.out.println("Test - pierwszy test jednostkowy:");
+        System.out.println();
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("*************************************************");
+        System.out.println("Testing Calculator class");
+        System.out.println();
+        Calculator calc = new Calculator();
+        int addResult = calc.add(12, 3);
+        int subResult = calc.subtract(11, 13);
+        if (addResult == 15 && subResult == -2) {
+            System.out.println("Calculator test OK");
+        } else {
+            System.out.println("Calculator has error!");
+        }
+
+
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/Calculator.java
@@ -0,0 +1,16 @@
+package com.kodilla.testing.user;
+
+public class Calculator {
+    int resultOfAdd, resultOfSub;
+    public int add (int firstAdd,int secondAdd){
+
+        return resultOfAdd = firstAdd + secondAdd;
+
+    }
+
+    public int subtract (int firstSubs,int secondSubs){
+
+        return resultOfSub = firstSubs - secondSubs;
+
+    }
+}
