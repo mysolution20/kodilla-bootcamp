89e5ce8c7c2349042fa32652495cd7e0495db991
mysolution20 <mysolution@op.pl>
2020-04-09 11:11:23 +0200

    Adding new Module kodilla-temp

---
+++ kodilla-temp/src/main/java/com/kodilla/FirstClass.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+
+public class FirstClass {
+
+    public void statementTest(){
+        System.out.println();
+        System.out.println("*** Testowy komunikat  4321 1234****") ;
+        System.out.println();
+    }
+
+
+}
---
+++ kodilla-temp/src/main/java/com/kodilla/KodillaCourseApplication.java
@@ -0,0 +1,10 @@
+package com.kodilla;
+
+public class KodillaCourseApplication {
+    public static void main(String[] args) {
+
+        FirstClass firstClass = new FirstClass();
+        firstClass.statementTest();
+
+    }
+}
