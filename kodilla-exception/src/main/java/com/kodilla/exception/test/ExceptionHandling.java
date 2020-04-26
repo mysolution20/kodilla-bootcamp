7ae21f057d912821d708806219147678dae34cf9
mysolution20 <mysolution@op.pl>
2020-04-26 11:15:14 +0200

    ex.nr: 8.3 - improvement

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,7 +1,6 @@
 package com.kodilla.exception.test;
 
 public class ExceptionHandling extends Exception {
-
     public void exceptionHandlingSimpleText() {
         try {
             System.out.println("*****1* When something went wrong then, there is pace to report about this problem.");
@@ -10,6 +9,17 @@ public class ExceptionHandling extends Exception {
                     "going to hands by general info: " + e + ".");
         } finally {
             System.out.println("*****2* Here, there is information that occurs always when problem appear.");
+        }
+    }
+
+    public static void main(String[] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try {
+            System.out.println(secondChallenge.probablyIWillThrowException(3, 1));
+        } catch (ExceptionHandling ex) {
+            ex.exceptionHandlingSimpleText();
+        } finally {
+            System.out.println("*****3* Here, there is information that always occurs about keeping order by exceptionHandlingSimpleText method.");
         }
     }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
@@ -7,17 +7,6 @@ public class SecondChallenge {
         }
         return "Done particular method 123321!";
     }
-
-    public static void main(String[] args) {
-        SecondChallenge secondChallenge = new SecondChallenge();
-        try {
-            System.out.println(secondChallenge.probablyIWillThrowException(3, 1));
-        } catch (ExceptionHandling ex) {
-            ex.exceptionHandlingSimpleText();
-        } finally {
-            System.out.println("*****3* Here, there is information that always occurs about keeping order by exceptionHandlingSimpleText method.");
-        }
-    }
 }
 
 
