8bc506d9685b7e6ad63cf7f3b98989f8a6d5ab0f
mysolution20 <mysolution@op.pl>
2020-04-29 20:38:30 +0200

    Exercise No. 7.7

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -4,37 +4,26 @@ import java.util.Scanner;
 
 public class RpsRunner {
     public static void main(String[] args) {
-
-        int won = 0, draws = 0, games = 0;
-        int opponent = 0, winner = 0, lost = 0;
-
-        final int playerSwitch = 1, opponentSwitch = 2, drawSwitch = 3;
-        final int stoneResult = 1, paperResult = 2, scissorsResult = 3;
-
-        Scanner choiceOption = new Scanner(System.in);
-        Scanner howManyWinsPlay = new Scanner(System.in);
-        Scanner nameOfOpponent = new Scanner(System.in);
+        int won = 0, draws = 0, games = 0, opponent = 0, winner = 0, lost = 0;
+        final int playerSwitch = 1, opponentSwitch = 2, drawSwitch = 3, stoneResult = 1, paperResult = 2, scissorsResult = 3;
+        Scanner loneHardworkingScanner = new Scanner(System.in);
 
         System.out.print("Jak masz na imie?");
-        String nameOfOpp = nameOfOpponent.next();
-//        String nameOfOpp = "Andrzej";
-
+        String nameOfOpp = loneHardworkingScanner.next();             //        String nameOfOpp = "Andrzej";
         System.out.print("Na imie masz: " + nameOfOpp + ", super!");
-        System.out.print(" ");
-
+        System.out.print("\n");
         String playAgain = "x";
-        do {
-            System.out.print("Podaj liczbe, do ilu zwycienstw gramy ?");
-            int playUntil = howManyWinsPlay.nextInt();
-//            int playUntil = 2;
 
-            System.out.print("Gramy do: " + playUntil + " wygranych.  ");
+        do {
+            System.out.print("Podaj liczbe, do ilu zwycienstw gramy ?\n");
+            int playUntil = loneHardworkingScanner.nextInt();        //            int playUntil = 2;
+
+            System.out.print("Gramy do: " + playUntil + " wygranych.  \n");
             do {
                 opponent = (int) (Math.random() * 3) + 1;
                 System.out.print("Losuj jeden z trzech, gdzie: " + "1 = kamień" + ",2 = papier" + ",3 = nożyce");
 
-                int player = choiceOption.nextInt();
-
+                int player = loneHardworkingScanner.nextInt();
                 System.out.print(nameOfOpp + " wybrałeś:");
 
                 switch (player) {
@@ -49,7 +38,6 @@ public class RpsRunner {
                         break;
                 }
                 System.out.print("Ja wybrałem:");
-
                 switch (opponent) {
                     case stoneResult:
                         System.out.println(" kamień");
@@ -88,17 +76,17 @@ public class RpsRunner {
                 if (winner == playerSwitch) {
                     won++;
                     System.out.println("W rundzie " + games + " wygrałeś: " + nameOfOpp + "!");
-                    System.out.println();
+                    System.out.println("\n");
 
                 } else if (winner == opponentSwitch) {
                     lost++;
                     System.out.println("W rundzie " + games + ": ja wygrałem !");
-                    System.out.println();
+                    System.out.println("\n");
 
                 } else {
                     draws++;
                     System.out.println("W rundzie " + games + ": mamy remis!");
-                    System.out.println();
+                    System.out.println("\n");
                 }
             }
             while (won < playUntil && lost < playUntil);
@@ -109,7 +97,7 @@ public class RpsRunner {
             } else
 
                 System.out.println("Ja wygrałem!");
-            System.out.println();
+            System.out.println("\n");
             System.out.println("Wygrałeś " + won + " razy.");
             System.out.println("Przegrałeś: " + lost + " razy.");
             System.out.println("Zremisowaliśmy " + draws + " razy.");
@@ -120,8 +108,8 @@ public class RpsRunner {
             draws = 0;
 
             System.out.print("Czy na pewno zakończyć grę? (gdy, tak = x,  n = nie)");
-            playAgain = choiceOption.nextLine();
-            playAgain = choiceOption.nextLine();
+            playAgain = loneHardworkingScanner.nextLine();
+            playAgain = loneHardworkingScanner.nextLine();
 
         } while (playAgain.equalsIgnoreCase("N"));
     }
