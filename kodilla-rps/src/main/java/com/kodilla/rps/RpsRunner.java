541e60337d6f10b86cb2c8e701c0e252bbd883e3
mysolution20 <mysolution@op.pl>
2020-04-30 21:28:57 +0200

    Ex.no 7.7 improvements have implemented

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -9,30 +9,32 @@ public class RpsRunner {
         Scanner loneHardworkingScanner = new Scanner(System.in);
         System.out.print("Jak masz na imie?");
         String nameOfOpp = loneHardworkingScanner.next();             //        String nameOfOpp = "Andrzej";
-        System.out.print("Na imie masz: " + nameOfOpp + ", super!");
-        System.out.print("\n");
+        System.out.print("Na imie masz: " + nameOfOpp + ", super!\n");
         String playAgain = "x";
         do {
-            System.out.print("Podaj liczbe, do ilu zwyciestw gramy ?");
-            System.out.print("\n");
+            System.out.print("Podaj liczbe, do ilu zwyciestw gramy ?\n");
             int playUntil = loneHardworkingScanner.nextInt();        //            int playUntil = 2;
-            System.out.print("Gramy do: " + playUntil + " wygranych.");
-            System.out.print("\n");
+            System.out.print("Gramy do: " + playUntil + " wygranych.\n");
             do {
                 opponent = (int) (Math.random() * 3) + 1;
-                System.out.print("Losuj jeden z trzech, gdzie: " + "1 = kamień" + ",2 = papier" + ",3 = nożyce");
+                System.out.print("Losuj jeden z trzech, gdzie: " + "1 = kamień" + ",2 = papier" + ",3 = nożyce.\n");
                 int player = loneHardworkingScanner.nextInt();
-                System.out.print(nameOfOpp + " wybrałeś:");
-                switch (player) {
-                    case stoneResult:
-                        System.out.println(" kamień");
-                        break;
-                    case paperResult:
-                        System.out.println(" papier");
-                        break;
-                    case scissorsResult:
-                        System.out.println(" nożyce");
-                        break;
+                if (!(player == 1 || player == 2 || player == 3)) {
+                    System.out.print(nameOfOpp + " wybrałeś: " + player + " to wartość z poza zakresu, kończymy losowanie.\n");
+                    System.exit(0);
+                } else {
+                    System.out.print(nameOfOpp + " wybrałeś: " + player);
+                    switch (player) {
+                        case stoneResult:
+                            System.out.println(" kamień");
+                            break;
+                        case paperResult:
+                            System.out.println(" papier");
+                            break;
+                        case scissorsResult:
+                            System.out.println(" nożyce");
+                            break;
+                    }
                 }
                 System.out.print("Ja wybrałem:");
                 switch (opponent) {
@@ -65,6 +67,7 @@ public class RpsRunner {
                         } else {
                             winner = drawSwitch;
                         }
+                        break;
                 }
                 games++;
                 if (winner == playerSwitch) {
@@ -75,23 +78,25 @@ public class RpsRunner {
                     System.out.println("W rundzie " + games + ": ja wygrałem !");
                 } else {
                     draws++;
-                    System.out.println("W rundzie " + games + ": mamy remis!");
+                    System.out.println("W rundzie " + games + ": mamy remis!\n");
                 }
             }
             while (won < playUntil && lost < playUntil);
-            System.out.println("Wszytkich losowań mieliśmy: " + games + " razy.");
+            System.out.println("*******************************************\n" +
+                    "Wszytkich losowań mieliśmy: " + games + " razy.\n" +
+                    "*******************************************");
             if (won > lost) {
                 System.out.println(nameOfOpp + " wygrałeś!");
             } else
                 System.out.println("Ja wygrałem!");
             System.out.println("Wygrałeś " + won + " razy.");
             System.out.println("Przegrałeś: " + lost + " razy.");
-            System.out.println("Zremisowaliśmy " + draws + " razy.");
+            System.out.println("Zremisowaliśmy " + draws + " razy.\n");
             games = 0;
             won = 0;
             lost = 0;
             draws = 0;
-            System.out.print("Czy na pewno zakończyć grę? (gdy, tak = x,  n = nie)");
+            System.out.print("Czy na pewno zakończyć grę? (gdy, tak = x,  n = nie).");
             playAgain = loneHardworkingScanner.nextLine();
             playAgain = loneHardworkingScanner.nextLine();
         } while (playAgain.equalsIgnoreCase("N"));
