9f6f8a5260a0280fc37dddd35fadb216059c9a98
mysolution20 <mysolution@op.pl>
2020-06-03 16:57:36 +0200

    initial commit to good patterns

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challanges/GoodPatternsMain.java
@@ -0,0 +1,13 @@
+package com.kodilla.good.patterns.challanges;
+
+public class GoodPatternsMain {
+
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+        System.out.println(movieStore.getMovies());
+
+
+
+    }
+}
--- kodilla-temp/src/main/java/com/kodilla/FirstClass.java
+++ kodilla-temp/src/main/java/com/kodilla/FirstClass.java
@@ -1,12 +1,9 @@
 package com.kodilla;
 
 public class FirstClass {
-
     public void statementTest(){
         System.out.println();
         System.out.println("*** Testowy komunikat  4321 1234****") ;
         System.out.println();
     }
-
-
 }
