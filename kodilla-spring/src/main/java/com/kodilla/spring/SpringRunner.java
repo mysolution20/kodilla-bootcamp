bac6ee0ab575071cd58d4d1daffeed02a9ca2e5d
mysolution20 <mysolution@op.pl>
2020-06-09 21:01:06 +0200

    commit to improve settings

--- kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
@@ -3,12 +3,10 @@ package com.kodilla.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static org.springframework.boot.SpringApplication.*;
-
 @SpringBootApplication
 public class SpringRunner {
     public static void main(String[] args) {
-        run(SpringRunner.class, args);
+        SpringApplication.run(SpringRunner.class,args);
 
     }
 }
