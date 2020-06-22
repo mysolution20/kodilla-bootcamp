0cdd8cbd4309612ce3ffa43dd6944997c8f895be
mysolution20 <62807367+mysolution20@users.noreply.github.com>
2020-06-22 18:29:11 +0200

    to overview draft sudoku

--- kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuBackTrackBoard.java
+++ kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuBackTrackBoard.java
@@ -3,8 +3,7 @@ package com.kodilla.sudoku;
 public class SudokuBackTrackBoard {
     SudokuBoard sudokuBoard = new SudokuBoard();
     SudokuBoard deepClonedBackTrackBoard = null;
-/**  Supose to overthink how to use it to thater ran of program
-      TODO: overthink how to use it to allow run of deepCloned method
+/**   TODO: overthink how to use it to allow run of deepCloned method
         try {
             deepClonedBackTrackBoard = sudokuBoard.deepCopy();
         } catch (CloneNotSupportedException e) {
