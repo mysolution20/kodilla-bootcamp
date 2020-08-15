edcfb29f12730000f82e364dc88541071f95101f
mysolution20 <62807367+mysolution20@users.noreply.github.com>
2020-08-15 20:38:26 +0200

    moduł 24.1 problem with @Test pakiet:  kodilla-patterns2

---
+++ kodilla-patterns2/src/test/java/com/kodillla/patterns2/KodillaPatterns2ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kodillla.patterns2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaPatterns2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/tempPackage/Main.java
@@ -0,0 +1,7 @@
+package com.kodilla.stream.tempPackage;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
