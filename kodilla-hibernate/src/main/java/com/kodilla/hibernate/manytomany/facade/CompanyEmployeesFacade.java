99d3ca84e2aa46d614b73426d9be8477f954810a
mysolution20 <62807367+mysolution20@users.noreply.github.com>
2020-08-16 09:36:52 +0200

    moduł 24.1 Budujemy fasadę do odczytu, zapisu list zadań - to review

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/CompanyEmployeesFacade.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/CompanyEmployeesFacade.java
@@ -1,25 +1,42 @@
 package com.kodilla.hibernate.manytomany.facade;
 
+import com.kodilla.hibernate.manytomany.Company;
+import com.kodilla.hibernate.manytomany.Employee;
 import com.kodilla.hibernate.manytomany.dao.CompanyDao;
 import com.kodilla.hibernate.manytomany.dao.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class CompanyEmployeesFacade  {
+public class CompanyEmployeesFacade {
     @Autowired
     private CompanyDao companyDao;
     @Autowired
     private EmployeeDao employeeDao;
 
-    public CompanyEmployeesFacade() {
+    public void companyFacadeSave(Company company) {
+        companyDao.save(company);
     }
 
-    public CompanyDao getCompanyDao() {
-        return companyDao;
+    public void employeeFacaseSave(Employee employee) {
+        employeeDao.save(employee);
     }
 
-    public EmployeeDao getEmployeeDao() {
-        return employeeDao;
+    public List<Company> getCompaniesWithFragmentOfItsName(String fragmentCompName) {
+        return companyDao.retrieveCompaniesWithFragmentOfItsName(fragmentCompName);
+    }
+
+    public List<Employee> getEmployeesWithFragmentOfName(String fragmentOfLastname) {
+        return employeeDao.retrieveEmployeesWithFragmentOfName(fragmentOfLastname);
+    }
+
+    public void deleteAllEmployee() {
+        employeeDao.deleteAll();
+    }
+
+    public void deleteAllCompany() {
+        companyDao.deleteAll();
     }
 }
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/facade/CompanyEmployeesFacadeTest.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/facade/CompanyEmployeesFacadeTest.java
@@ -29,20 +29,18 @@ public class CompanyEmployeesFacadeTest {
         janKowalki.getCompanies().add(biedronka);
 
         //When
-//        companyEmployeesFacade
-        companyEmployeesFacade.getCompanyDao().save(biedronka);
-        companyEmployeesFacade.getEmployeeDao().save(janKowalki);
+        companyEmployeesFacade.companyFacadeSave(biedronka);
+        companyEmployeesFacade.employeeFacaseSave(janKowalki);
 
-        List<Company> companiesWithFragmentOfItsName = companyEmployeesFacade.getCompanyDao().retrieveCompaniesWithFragmentOfItsName("edro");// Biedronka
-        List<Employee> employeesWithFragmentOfName = companyEmployeesFacade.getEmployeeDao().retrieveEmployeesWithFragmentOfName("walk");// Kowalki
-
+        List<Company> companiesWithFragmentOfItsName = companyEmployeesFacade.getCompaniesWithFragmentOfItsName("edro"); // biedronka
+        List<Employee> employeesWithFragmentOfName = companyEmployeesFacade.getEmployeesWithFragmentOfName("walk");// Kowalki
         //Then
         Assert.assertEquals(1, companiesWithFragmentOfItsName.size());
         Assert.assertEquals(1, employeesWithFragmentOfName.size());
 
         try {
-            companyEmployeesFacade.getCompanyDao().deleteAll();
-            companyEmployeesFacade.getEmployeeDao().deleteAll();
+            companyEmployeesFacade.deleteAllEmployee();
+            companyEmployeesFacade.deleteAllCompany();
         } finally {
             //CleanUp
             System.out.println("All records have cleaned up by: companyEmployeesFacade");
